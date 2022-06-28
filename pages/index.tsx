aaaddf8e6fa284f2f3c0938a207800a1ccda9fa0
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-28 01:07:47 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import MusicCampaigns from "../components/MusicCampaigns";
 import AboutMe from "./aboutMe";
 import Landing from "./landing";
 import Projects from "./projects";
