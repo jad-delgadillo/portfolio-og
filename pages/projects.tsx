4b0b883be54b532a999bfc30e2b055406f4050cd
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-28 01:15:03 -0500

    update

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import MusicCampaigns from "../components/MusicCampaigns";
+import MusicCampaigns from "../components/musicCampaigns";
 import data from "../src/dataCamapigns";
 
 function Projects() {
